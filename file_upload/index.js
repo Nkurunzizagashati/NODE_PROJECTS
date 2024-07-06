a8b784239333e56b8e9edd866d08cc2eccea7120
Fabrice <f.nkurunziz@alustudent.com>
2024-07-06 23:10:49 +0200

    created an express app

--- file_upload/index.js
+++ file_upload/index.js
@@ -1,1 +1,13 @@
 import express from "express";
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home Page", message: "Hello, EJS!" });
+});
+
+app.listen(3000, () => {
+  console.log("SERVER IS STARTED AND IS RUNNING ON PORT: 3000");
+});
