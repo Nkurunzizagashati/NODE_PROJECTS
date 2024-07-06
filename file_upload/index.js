c42dcf06db5f5a3c700b6d1c197b53a3974fbd32
Fabrice <f.nkurunziz@alustudent.com>
2024-07-06 23:39:20 +0200

    add functionality for uploading an image

--- file_upload/index.js
+++ file_upload/index.js
@@ -1,14 +1,29 @@
 import express from "express";
 import path from "path";
+import multer from "multer";
 
 const app = express();
 app.set("view engine", "ejs");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(file);
+    cb(null, "Images");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, Date.now() + "_" + path.basename(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Home Page", message: "Hello, EJS!" });
 });
 
-app.post("/upload", (req, res) => {
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log("Hello from the server");
   res.send("Uploaded");
 });
 
