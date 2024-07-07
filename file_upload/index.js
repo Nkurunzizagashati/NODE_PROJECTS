bc5daad0b41011c7d944f5114fdf8402e9073d67
Fabrice <f.nkurunziz@alustudent.com>
2024-07-07 23:53:02 +0200

    add google oauth20 authentication

--- file_upload/index.js
+++ file_upload/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   session({
-    secret: "My secret",
+    secret: process.env.SESSION_SECRET,
     saveUninitialized: false,
     resave: false,
 
