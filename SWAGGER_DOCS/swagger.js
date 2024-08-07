cd5ddeecd9a75878f5b8b17b2efbb04438e4b9ca
Fabrice <f.nkurunziz@alustudent.com>
2024-08-07 15:09:01 +0200

    swagger documentation

---
+++ SWAGGER_DOCS/swagger.js
@@ -0,0 +1,16 @@
+const options = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'API Documentation',
+			version: '1.0.0',
+			description: 'This is a sample API documentation',
+		},
+		servers: [
+			{
+				url: 'http://localhost:3000',
+				description: 'Development server',
+			},
+		],
+	},
+};
